0d904c8a4220f4faa83b44059b82b3290f86d762
Feruz Kabilov <f_kabilov@zenk.co.jp>
2021-07-02 22:34:53 +0900

    Third

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,26 +12,37 @@
         <nav class="p-6 bg-white flex justify-between mb-6">
             <ul class="flex items-center">
                 <li>
-                    <a href="http://" class="p-3">Home</a>
+                    <a href="/" class="p-3">Home</a>
                 </li>
                 <li>
-                    <a href="http://" class="p-3">Dashboard</a>
+                    <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
                 </li>
                 <li>
-                    <a href="http://" class="p-3">Posts</a>
+                    <a href="{{ route('posts') }}" class="p-3">Posts</a>
                 </li>
-            </ul>  
-            
+            </ul>
+
             <ul class="flex items-center">
-                <li>
-                    <a href="http://" class="p-3">Roma </a>
-                </li>
-                <li>
-                    <a href="http://" class="p-3">Login</a>
-                </li>
-                <li>
-                    <a href="{{ route('register') }}" class="p-3">Regesiter</a>
-                </li>
+                @auth
+                    <li>
+                        <a href="" class="p-3">{{ auth()->user()->name }}</a>
+                    </li>
+                    <li>
+                        <form action="{{ route('logout') }}" method="post" class="p-3 inline">
+                            @csrf
+                            <button type="submit">Logout</button>
+                        </form>
+                    </li>
+                @endauth
+
+                @guest
+                    <li>
+                        <a href="{{ route('login') }}" class="p-3">Login</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('register') }}" class="p-3">Register</a>
+                    </li>
+                @endguest
             </ul>
         </nav>
         @yield('content')
